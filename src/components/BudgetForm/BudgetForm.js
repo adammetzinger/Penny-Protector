5caba2b549ce02780cec79f108619f82ebf3324e
adammetzinger <adamcm728@gmail.com>
2024-02-28 18:08:27 -0600

    BudgetForm built

---
+++ src/components/BudgetForm/BudgetForm.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function BudgetForm({ handleCreateBudget }) {
+    const [newBudget, setNewBudget] = useState({
+        title: '',
+        date: '',
+        budget: '',
+    })
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        handleCreateBudget(newBudget);
+        setNewBudget({
+            title: '',
+            date: '',
+            budget: '',
+        })
+    }
+
+    function handleChange(evt) {
+        const formNewBudget = {...newBudget, [evt.target.name]: evt.target.value};
+        setNewBudget(formNewBudget);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            name='title'
+            value={newBudget.title}
+            onChange={handleChange}
+            required
+            />
+            <input 
+            name='date'
+            value={newBudget.date}
+            onChange={handleChange}
+            required
+            />
+            <input 
+            name='budget'
+            value={newBudget.budget}
+            onChange={handleChange}
+            required
+            />
+        </form>
+    )
+}
